e85bdb8623d7c686b6c50fc7af0dfade7c6af962
James D. Trotter <james@simula.no>
2019-08-13 11:35:43 +0200

    Add numactl-2.0.12

---
+++ numactl/gcc/build-numactl-2.0.12.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Package details
+PKG_NAME=numactl
+PKG_VERSION=2.0.12
+MODULEFILE=$(realpath ${PKG_VERSION})
+SRC_URL=https://github.com/numactl/numactl/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz
+SRC_DIR=${PKG_NAME}-${PKG_VERSION}
+
+# Set up installation paths
+INSTALL_DIR=/cm/shared/apps
+MODULEFILES_DIR=/cm/shared/modulefiles
+PKG_MODULE=${PKG_NAME}/gcc/${PKG_VERSION}
+PKG_INSTALL_DIR=${INSTALL_DIR}/${PKG_MODULE}
+PKG_MODULE_DIR=$(dirname ${MODULEFILES_DIR}/${PKG_MODULE})
+
+# Set up build and temporary install directories
+OUT_DIR=$(mktemp -d -t ${PKG_NAME}-${PKG_VERSION}-XXXXXX)
+BUILD_DIR=${OUT_DIR}/build
+DESTDIR=${OUT_DIR}
+mkdir -p ${BUILD_DIR}
+mkdir -p ${DESTDIR}
+
+# Download package
+SRC_PKG=${BUILD_DIR}/${PKG_NAME}-${PKG_VERSION}.tar.gz
+curl -Lo ${SRC_PKG} ${SRC_URL}
+
+# Unpack
+tar -C ${BUILD_DIR} -xzvf ${SRC_PKG}
+
+# Build
+pushd ${BUILD_DIR}/${SRC_DIR}
+./configure --prefix=${PKG_INSTALL_DIR}
+make -j 8
+make install DESTDIR=${DESTDIR}
+popd
+
+
+echo "To install ${PKG_NAME}-${PKG_VERSION}:"
+echo "  mkdir -p ${PKG_INSTALL_DIR}"
+echo "  cp -r ${DESTDIR}/${PKG_INSTALL_DIR}/* ${PKG_INSTALL_DIR}"
+echo "  mkdir -p ${PKG_MODULE_DIR}"
+echo "  cp ${MODULEFILE} ${PKG_MODULE_DIR}"
