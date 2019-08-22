e3e409bb8e9ba56483da4c5b1ef32c6f5ee918ab
James D. Trotter <james@simula.no>
2019-08-22 19:46:22 +0200

    Add a script to build all modules

---
+++ build-all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+#
+# Build all modules
+#
+# Example usage: Build and install modules and module files to the
+# current user's home directory:
+#
+#   ./build.sh --prefix=$HOME
+#
+# The newly installed modules can be made available by executing the
+# following command:
+#
+#   module use $HOME/modulefiles
+#
+#
+
+build_files=$(find modules -name build.sh)
+modules=$(
+    for f in ${build_files}; do
+	echo "${f}" | sed -e "s/^modules\///" -e "s/\/build.sh$//";
+    done)
+
+./build.sh "$@" ${modules}
