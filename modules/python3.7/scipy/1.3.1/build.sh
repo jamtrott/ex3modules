107c6cabf5308fd048c225602ddf09c20f2a756e
James D. Trotter <james@simula.no>
2019-09-15 01:52:46 +0200

    python3.7/scipy: Build from source
    This is needed to build scipy for aarch64.

--- modules/python3.7/scipy/1.3.1/build.sh
+++ modules/python3.7/scipy/1.3.1/build.sh
@@ -23,6 +23,8 @@ PYTHON_VERSION_SHORT=3.7
 pkg_moduledir="python${PYTHON_VERSION_SHORT}/${pkg_name}/${pkg_version}"
 pkg_description="Fundamental package for scientific computing with Python"
 pkg_url="https://www.scipy.org/"
+src_url="https://github.com/scipy/scipy/archive/v${pkg_version}.tar.gz"
+src_dir="${pkg_name}-${pkg_version}"
 
 function main()
 {
@@ -40,9 +42,32 @@ function main()
     module_load_build_deps build_deps
     pkg_prereqs=$(module_prereqs prereqs)
 
-    # Download and install from pip
+    # Download and unpack source
     pkg_prefix=$(module_build_prefix "${prefix}" "${pkg_moduledir}")
-    python3 -m pip install --prefix="${pkg_prefix}" $([ ! -z "${DESTDIR}" ] && --root="${DESTDIR}") "${pkg_name}"=="${pkg_version}"
+    pkg_build_dir=$(module_build_create_build_dir "${pkg_name}" "${pkg_version}")
+    pkg_src="${pkg_build_dir}/$(basename ${src_url})"
+    module_build_download_package "${src_url}" "${pkg_src}"
+    module_build_unpack "${pkg_src}" "${pkg_build_dir}"
+
+    # Build
+    pushd "${pkg_build_dir}/${src_dir}"
+
+    cat >site.cfg <<EOF
+[openblas]
+libraries = ${BLASLIB}
+library_dirs = ${OPENBLAS_LIBDIR}
+include_dirs = ${OPENBLAS_INCDIR}
+runtime_library_dirs = ${OPENBLAS_LIBDIR}
+EOF
+
+    # The installation directory must be added to PYTHONPATH before installing
+    PYTHONPATH="${PYTHONPATH}:${DESTDIR}${pkg_prefix}/lib/python${PYTHON_VERSION_SHORT}/site-packages"
+    mkdir -p "${DESTDIR}${pkg_prefix}/lib/python${PYTHON_VERSION_SHORT}/site-packages"
+    python3 setup.py build
+    python3 setup.py install \
+	    --prefix="${pkg_prefix}" \
+	    $(! [ -z "${DESTDIR}" ] && --root="${DESTDIR}")
+    popd
 
     # Write the module file
     pkg_modulefile=$(module_build_modulefile "${prefix}" "${modulefilesdir}" "${pkg_moduledir}")
