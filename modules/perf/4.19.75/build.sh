49d0e796ed37b620fd861423702e2f29cabbfdc4
James D. Trotter <james@simula.no>
2019-09-24 16:04:21 +0200

    Add perf-4.19.75

---
+++ modules/perf/4.19.75/build.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+#
+# Build perf
+#
+# The following command will build the module, write a module file,
+# and install them to the directory 'modules' in your home directory:
+#
+#   build.sh --prefix=$HOME/modules 2>&1 | tee build.log
+#
+# The module can then be loaded as follows:
+#
+#   module use $HOME/modules/modulefiles
+#   module load perf
+#
+set -o errexit
+
+. ../../../common/module.sh
+
+pkg_name=perf
+pkg_version=4.19.75
+pkg_moduledir="${pkg_name}/${pkg_version}"
+pkg_description="Performance analysis tools for Linux"
+pkg_url="https://perf.wiki.kernel.org/index.php/Main_Page"
+src_url="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-${pkg_version}.tar.xz"
+src_dir="linux-${pkg_version}/tools/perf"
+
+function main()
+{
+    # Parse program options
+    module_build_parse_command_line_args \
+	"${0}" \
+	"${pkg_name}" \
+	"${pkg_version}" \
+	"${pkg_moduledir}" \
+	"${pkg_description}" \
+	"${pkg_url}" \
+	"$@"
+
+    # Load build-time dependencies and determine prerequisite modules
+    module_load_build_deps build_deps
+    pkg_prereqs=$(module_prereqs prereqs)
+
+    # Download and unpack source
+    pkg_prefix=$(module_build_prefix "${prefix}" "${pkg_moduledir}")
+    pkg_build_dir=$(module_build_create_build_dir "${pkg_name}" "${pkg_version}")
+    pkg_src="${pkg_build_dir}/$(basename ${src_url})"
+    module_build_download_package "${src_url}" "${pkg_src}"
+    module_build_unpack "${pkg_src}" "${pkg_build_dir}" -x
+
+    # Build
+    pushd "${pkg_build_dir}/${src_dir}"
+    make -j"${JOBS}"
+    make prefix="${pkg_prefix}" install
+    popd
+
+    # Write the module file
+    pkg_modulefile=$(module_build_modulefile "${prefix}" "${modulefilesdir}" "${pkg_moduledir}")
+    cat >"${pkg_modulefile}" <<EOF
+#%Module
+# ${pkg_name} ${pkg_version}
+
+proc ModulesHelp { } {
+     puts stderr "\tSets up the environment for ${pkg_name} ${pkg_version}\n"
+}
+
+module-whatis "${pkg_description}"
+module-whatis "${pkg_url}"
+
+${pkg_prereqs}
+
+set MODULES_PREFIX [getenv MODULES_PREFIX ""]
+setenv ${pkg_name^^}_ROOT \$MODULES_PREFIX${pkg_prefix}
+setenv ${pkg_name^^}_INCDIR \$MODULES_PREFIX${pkg_prefix}/include
+setenv ${pkg_name^^}_INCLUDEDIR \$MODULES_PREFIX${pkg_prefix}/include
+setenv ${pkg_name^^}_LIBDIR \$MODULES_PREFIX${pkg_prefix}/lib
+setenv ${pkg_name^^}_LIBRARYDIR \$MODULES_PREFIX${pkg_prefix}/lib
+prepend-path PATH \$MODULES_PREFIX${pkg_prefix}/bin
+prepend-path LIBRARY_PATH \$MODULES_PREFIX${pkg_prefix}/lib
+prepend-path LD_LIBRARY_PATH \$MODULES_PREFIX${pkg_prefix}/lib
+set MSG "${pkg_name} ${pkg_version}"
+EOF
+
+    module_build_cleanup "${pkg_build_dir}"
+}
+
+main "$@"
