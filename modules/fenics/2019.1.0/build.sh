6dec4aefc2ce92a4c4465c3e47c3d7b7459d86d7
James D. Trotter <james@simula.no>
2019-10-20 16:01:53 +0200

    modules: Separate build script from module description

--- modules/fenics/2019.1.0/build.sh
+++ modules/fenics/2019.1.0/build.sh
@@ -13,50 +13,5 @@
 #   module load fenics
 #
 set -o errexit
-
-pkg_name=fenics
-pkg_version=2019.1.0
-pkg_moduledir="${pkg_name}/${pkg_version}"
-pkg_description="Computing platform for solving partial differential equations"
-pkg_url="https://fenicsproject.org"
-
-function main()
-{
-    . ../../../common/module.sh
-
-    # Parse program options
-    module_build_parse_command_line_args \
-	"${0}" \
-	"${pkg_name}" \
-	"${pkg_version}" \
-	"${pkg_moduledir}" \
-	"${pkg_description}" \
-	"${pkg_url}" \
-	"$@"
-
-    # Load build-time dependencies and determine prerequisite modules
-    module_load_build_deps build_deps
-    pkg_prereqs=$(module_prereqs prereqs)
-
-    # Nothing to build
-
-    # Write the module file
-    pkg_modulefile=$(module_build_modulefile "${prefix}" "${modulefilesdir}" "${pkg_moduledir}")
-    cat >"${pkg_modulefile}" <<EOF
-#%Module
-# ${pkg_name} ${pkg_version}
-
-proc ModulesHelp { } {
-     puts stderr "\tSets up the environment for ${pkg_name} ${pkg_version}\n"
-}
-
-module-whatis "${pkg_description}"
-module-whatis "${pkg_url}"
-
-${pkg_prereqs}
-
-set MSG "${pkg_name} ${pkg_version}"
-EOF
-}
-
-main "$@"
+source build
+build "$@"
