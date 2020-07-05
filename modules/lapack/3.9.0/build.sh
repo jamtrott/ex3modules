eb3b5cf1c40f85277b0819ccb1415ba9baae7bbe
James D. Trotter <james@simula.no>
2020-07-05 13:42:28 +0200

    Add lapack/3.9.0

---
+++ modules/lapack/3.9.0/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Build scalapack
+#
+# The following command will build the module, write a module file,
+# and install them to the directory 'modules' in your home directory:
+#
+#   build.sh --prefix=$HOME/modules 2>&1 | tee build.log
+#
+# The module can then be loaded as follows:
+#
+#   module use $HOME/modules/modulefiles
+#   module load scalapack
+#
+set -o errexit
+source build
+build "$@"
