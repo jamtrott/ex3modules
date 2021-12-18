cebda263de31844acaec30952481c6c17b1a724b
James D. Trotter <james@simula.no>
2021-12-18 18:52:23 +0100

    numpy: Revert NPY_NUM_BUILD_JOBS
    This fails if '$(JOBS)' is empty

--- makefiles/python-numpy-1.19.2.mk
+++ makefiles/python-numpy-1.19.2.mk
@@ -124,7 +124,7 @@ $($(python-numpy)-prefix)/.pkginstall: $(modulefilesdir)/.markerfile $$(foreach 
 		$(MODULE) use $(modulefilesdir) && \
 		$(MODULE) load $($(python-numpy)-builddeps) && \
 		PYTHONPATH=$($(python-numpy)-site-packages):$${PYTHONPATH} \
-		NPY_NUM_BUILD_JOBS=$(JOBS) NPY_BLAS_ORDER=openblas,blas NPY_LAPACK_ORDER=openblas,lapack $(PYTHON) -m pip install . --no-deps --ignore-installed --prefix=$($(python-numpy)-prefix)
+		NPY_BLAS_ORDER=openblas,blas NPY_LAPACK_ORDER=openblas,lapack $(PYTHON) -m pip install . --no-deps --ignore-installed --prefix=$($(python-numpy)-prefix)
 	@touch $@
 
 $($(python-numpy)-modulefile): $(modulefilesdir)/.markerfile $($(python-numpy)-prefix)/.pkginstall
