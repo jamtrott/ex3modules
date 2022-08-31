af8c0bfa3ce8a22263cb7e1cccf072e502776746
James D. Trotter <james@simula.no>
2022-08-31 21:00:59 +0200

    fix to use superlu_dist-8.1.0 instead of defaulting to 6.4.0

--- makefiles/superlu_dist-32-6.4.0.mk
+++ makefiles/superlu_dist-32-6.4.0.mk
@@ -1,5 +1,5 @@
 # ex3modules - Makefiles for installing software on the eX3 cluster
-# Copyright (C) 2020 James D. Trotter
+# Copyright (C) 2022 James D. Trotter
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,43 +18,43 @@
 #
 # superlu_dist-32-6.4.0
 
-superlu_dist-32-version = 6.4.0
-superlu_dist-32 = superlu_dist-32-$(superlu_dist-32-version)
-$(superlu_dist-32)-description = MPI-based direct solver for large, sparse non-symmetric systems of equations in distributed memory
-$(superlu_dist-32)-url = https://github.com/xiaoyeli/superlu_dist/
-$(superlu_dist-32)-srcurl = https://github.com/xiaoyeli/superlu_dist/archive/v$(superlu_dist-32-version).tar.gz
-$(superlu_dist-32)-builddeps = $(cmake) $(blas) $(mpi) $(parmetis) $(combblas)
-$(superlu_dist-32)-prereqs = $(blas) $(mpi) $(parmetis) $(combblas)
-$(superlu_dist-32)-src = $($(superlu_dist-src)-src)
-$(superlu_dist-32)-srcdir = $(pkgsrcdir)/$(superlu_dist-32)
-$(superlu_dist-32)-builddir = $($(superlu_dist-32)-srcdir)/build
-$(superlu_dist-32)-modulefile = $(modulefilesdir)/$(superlu_dist-32)
-$(superlu_dist-32)-prefix = $(pkgdir)/$(superlu_dist-32)
+superlu_dist-32-6.4.0-version = 6.4.0
+superlu_dist-32-6.4.0 = superlu_dist-32-$(superlu_dist-32-6.4.0-version)
+$(superlu_dist-32-6.4.0)-description = MPI-based direct solver for large, sparse non-symmetric systems of equations in distributed memory
+$(superlu_dist-32-6.4.0)-url = https://github.com/xiaoyeli/superlu_dist/
+$(superlu_dist-32-6.4.0)-srcurl = https://github.com/xiaoyeli/superlu_dist/archive/v$(superlu_dist-32-6.4.0-version).tar.gz
+$(superlu_dist-32-6.4.0)-builddeps = $(cmake) $(blas) $(mpi) $(parmetis) $(combblas) $(cuda-toolkit)
+$(superlu_dist-32-6.4.0)-prereqs = $(blas) $(mpi) $(parmetis) $(combblas) $(cuda-toolkit)
+$(superlu_dist-32-6.4.0)-src = $($(superlu_dist-src-6.4.0)-src)
+$(superlu_dist-32-6.4.0)-srcdir = $(pkgsrcdir)/$(superlu_dist-32-6.4.0)
+$(superlu_dist-32-6.4.0)-builddir = $($(superlu_dist-32-6.4.0)-srcdir)/build
+$(superlu_dist-32-6.4.0)-modulefile = $(modulefilesdir)/$(superlu_dist-32-6.4.0)
+$(superlu_dist-32-6.4.0)-prefix = $(pkgdir)/$(superlu_dist-32-6.4.0)
 
-$($(superlu_dist-32)-srcdir)/.markerfile:
+$($(superlu_dist-32-6.4.0)-srcdir)/.markerfile:
 	$(INSTALL) -d $(dir $@) && touch $@
 
-$($(superlu_dist-32)-prefix)/.markerfile:
+$($(superlu_dist-32-6.4.0)-prefix)/.markerfile:
 	$(INSTALL) -d $(dir $@) && touch $@
 
-$($(superlu_dist-32)-prefix)/.pkgunpack: $$($(superlu_dist-32)-src) $($(superlu_dist-32)-srcdir)/.markerfile $($(superlu_dist-32)-prefix)/.markerfile $$(foreach dep,$$($(superlu_dist-32)-builddeps),$(modulefilesdir)/$$(dep))
-	tar -C $($(superlu_dist-32)-srcdir) --strip-components 1 -xz -f $<
+$($(superlu_dist-32-6.4.0)-prefix)/.pkgunpack: $$($(superlu_dist-32-6.4.0)-src) $($(superlu_dist-32-6.4.0)-srcdir)/.markerfile $($(superlu_dist-32-6.4.0)-prefix)/.markerfile $$(foreach dep,$$($(superlu_dist-32-6.4.0)-builddeps),$(modulefilesdir)/$$(dep))
+	tar -C $($(superlu_dist-32-6.4.0)-srcdir) --strip-components 1 -xz -f $<
 	@touch $@
 
-$($(superlu_dist-32)-prefix)/.pkgpatch: $(modulefilesdir)/.markerfile $$(foreach dep,$$($(superlu_dist-32)-builddeps),$(modulefilesdir)/$$(dep)) $($(superlu_dist-32)-prefix)/.pkgunpack
-	sed -i 's,set(CMAKE_CXX_STANDARD 11),set(CMAKE_CXX_STANDARD 14),' $($(superlu_dist-32)-srcdir)/CMakeLists.txt
+$($(superlu_dist-32-6.4.0)-prefix)/.pkgpatch: $(modulefilesdir)/.markerfile $$(foreach dep,$$($(superlu_dist-32-6.4.0)-builddeps),$(modulefilesdir)/$$(dep)) $($(superlu_dist-32-6.4.0)-prefix)/.pkgunpack
+	sed -i 's,set(CMAKE_CXX_STANDARD 11),set(CMAKE_CXX_STANDARD 14),' $($(superlu_dist-32-6.4.0)-srcdir)/CMakeLists.txt
 	@touch $@
 
-ifneq ($($(superlu_dist-32)-builddir),$($(superlu_dist-32)-srcdir))
-$($(superlu_dist-32)-builddir)/.markerfile: $($(superlu_dist-32)-prefix)/.pkgunpack
+ifneq ($($(superlu_dist-32-6.4.0)-builddir),$($(superlu_dist-32-6.4.0)-srcdir))
+$($(superlu_dist-32-6.4.0)-builddir)/.markerfile: $($(superlu_dist-32-6.4.0)-prefix)/.pkgunpack
 	$(INSTALL) -d $(dir $@) && touch $@
 endif
 
-$($(superlu_dist-32)-prefix)/.pkgbuild: $(modulefilesdir)/.markerfile $$(foreach dep,$$($(superlu_dist-32)-builddeps),$(modulefilesdir)/$$(dep)) $($(superlu_dist-32)-builddir)/.markerfile $($(superlu_dist-32)-prefix)/.pkgpatch
-	cd $($(superlu_dist-32)-builddir) && \
+$($(superlu_dist-32-6.4.0)-prefix)/.pkgbuild: $(modulefilesdir)/.markerfile $$(foreach dep,$$($(superlu_dist-32-6.4.0)-builddeps),$(modulefilesdir)/$$(dep)) $($(superlu_dist-32-6.4.0)-builddir)/.markerfile $($(superlu_dist-32-6.4.0)-prefix)/.pkgpatch
+	cd $($(superlu_dist-32-6.4.0)-builddir) && \
 		$(MODULESINIT) && \
 		$(MODULE) use $(modulefilesdir) && \
-		$(MODULE) load $($(superlu_dist-32)-builddeps) && \
+		$(MODULE) load $($(superlu_dist-32-6.4.0)-builddeps) && \
 		$(CMAKE) .. \
 			-DCMAKE_INSTALL_PREFIX=$($(superlu_dist-32)-prefix) \
 			-DCMAKE_INSTALL_LIBDIR=lib \
@@ -75,54 +75,54 @@ $($(superlu_dist-32)-prefix)/.pkgbuild: $(modulefilesdir)/.markerfile $$(foreach
 		$(MAKE) VERBOSE=1
 	@touch $@
 
-$($(superlu_dist-32)-prefix)/.pkgcheck: $(modulefilesdir)/.markerfile $$(foreach dep,$$($(superlu_dist-32)-builddeps),$(modulefilesdir)/$$(dep)) $($(superlu_dist-32)-builddir)/.markerfile $($(superlu_dist-32)-prefix)/.pkgbuild
+$($(superlu_dist-32-6.4.0)-prefix)/.pkgcheck: $(modulefilesdir)/.markerfile $$(foreach dep,$$($(superlu_dist-32-6.4.0)-builddeps),$(modulefilesdir)/$$(dep)) $($(superlu_dist-32-6.4.0)-builddir)/.markerfile $($(superlu_dist-32-6.4.0)-prefix)/.pkgbuild
 	@touch $@
 
-$($(superlu_dist-32)-prefix)/.pkginstall: $(modulefilesdir)/.markerfile $$(foreach dep,$$($(superlu_dist-32)-builddeps),$(modulefilesdir)/$$(dep)) $($(superlu_dist-32)-builddir)/.markerfile $($(superlu_dist-32)-prefix)/.pkgcheck
-	cd $($(superlu_dist-32)-builddir) && \
+$($(superlu_dist-32-6.4.0)-prefix)/.pkginstall: $(modulefilesdir)/.markerfile $$(foreach dep,$$($(superlu_dist-32-6.4.0)-builddeps),$(modulefilesdir)/$$(dep)) $($(superlu_dist-32-6.4.0)-builddir)/.markerfile $($(superlu_dist-32-6.4.0)-prefix)/.pkgcheck
+	cd $($(superlu_dist-32-6.4.0)-builddir) && \
 		$(MODULESINIT) && \
 		$(MODULE) use $(modulefilesdir) && \
-		$(MODULE) load $($(superlu_dist-32)-builddeps) && \
+		$(MODULE) load $($(superlu_dist-32-6.4.0)-builddeps) && \
 		$(MAKE) install
 	@touch $@
 
-$($(superlu_dist-32)-modulefile): $(modulefilesdir)/.markerfile $($(superlu_dist-32)-prefix)/.pkginstall
+$($(superlu_dist-32-6.4.0)-modulefile): $(modulefilesdir)/.markerfile $($(superlu_dist-32-6.4.0)-prefix)/.pkginstall
 	printf "" >$@
 	echo "#%Module" >>$@
-	echo "# $(superlu_dist-32)" >>$@
+	echo "# $(superlu_dist-32-6.4.0)" >>$@
 	echo "" >>$@
 	echo "proc ModulesHelp { } {" >>$@
-	echo "     puts stderr \"\tSets up the environment for $(superlu_dist-32)\\n\"" >>$@
+	echo "     puts stderr \"\tSets up the environment for $(superlu_dist-32-6.4.0)\\n\"" >>$@
 	echo "}" >>$@
 	echo "" >>$@
-	echo "module-whatis \"$($(superlu_dist-32)-description)\"" >>$@
-	echo "module-whatis \"$($(superlu_dist-32)-url)\"" >>$@
-	printf "$(foreach prereq,$($(superlu_dist-32)-prereqs),\n$(MODULE) load $(prereq))" >>$@
+	echo "module-whatis \"$($(superlu_dist-32-6.4.0)-description)\"" >>$@
+	echo "module-whatis \"$($(superlu_dist-32-6.4.0)-url)\"" >>$@
+	printf "$(foreach prereq,$($(superlu_dist-32-6.4.0)-prereqs),\n$(MODULE) load $(prereq))" >>$@
 	echo "" >>$@
 	echo "" >>$@
-	echo "setenv SUPERLU_DIST_ROOT $($(superlu_dist-32)-prefix)" >>$@
-	echo "setenv SUPERLU_DIST_INCDIR $($(superlu_dist-32)-prefix)/include" >>$@
-	echo "setenv SUPERLU_DIST_INCLUDEDIR $($(superlu_dist-32)-prefix)/include" >>$@
-	echo "setenv SUPERLU_DIST_LIBDIR $($(superlu_dist-32)-prefix)/lib" >>$@
-	echo "setenv SUPERLU_DIST_LIBRARYDIR $($(superlu_dist-32)-prefix)/lib" >>$@
-	echo "prepend-path PATH $($(superlu_dist-32)-prefix)/bin" >>$@
-	echo "prepend-path C_INCLUDE_PATH $($(superlu_dist-32)-prefix)/include" >>$@
-	echo "prepend-path CPLUS_INCLUDE_PATH $($(superlu_dist-32)-prefix)/include" >>$@
-	echo "prepend-path LIBRARY_PATH $($(superlu_dist-32)-prefix)/lib" >>$@
-	echo "prepend-path LD_LIBRARY_PATH $($(superlu_dist-32)-prefix)/lib" >>$@
-	echo "set MSG \"$(superlu_dist-32)\"" >>$@
+	echo "setenv SUPERLU_DIST_ROOT $($(superlu_dist-32-6.4.0)-prefix)" >>$@
+	echo "setenv SUPERLU_DIST_INCDIR $($(superlu_dist-32-6.4.0)-prefix)/include" >>$@
+	echo "setenv SUPERLU_DIST_INCLUDEDIR $($(superlu_dist-32-6.4.0)-prefix)/include" >>$@
+	echo "setenv SUPERLU_DIST_LIBDIR $($(superlu_dist-32-6.4.0)-prefix)/lib" >>$@
+	echo "setenv SUPERLU_DIST_LIBRARYDIR $($(superlu_dist-32-6.4.0)-prefix)/lib" >>$@
+	echo "prepend-path PATH $($(superlu_dist-32-6.4.0)-prefix)/bin" >>$@
+	echo "prepend-path C_INCLUDE_PATH $($(superlu_dist-32-6.4.0)-prefix)/include" >>$@
+	echo "prepend-path CPLUS_INCLUDE_PATH $($(superlu_dist-32-6.4.0)-prefix)/include" >>$@
+	echo "prepend-path LIBRARY_PATH $($(superlu_dist-32-6.4.0)-prefix)/lib" >>$@
+	echo "prepend-path LD_LIBRARY_PATH $($(superlu_dist-32-6.4.0)-prefix)/lib" >>$@
+	echo "set MSG \"$(superlu_dist-32-6.4.0)\"" >>$@
 
-$(superlu_dist-32)-src: $$($(superlu_dist-32)-src)
-$(superlu_dist-32)-unpack: $($(superlu_dist-32)-prefix)/.pkgunpack
-$(superlu_dist-32)-patch: $($(superlu_dist-32)-prefix)/.pkgpatch
-$(superlu_dist-32)-build: $($(superlu_dist-32)-prefix)/.pkgbuild
-$(superlu_dist-32)-check: $($(superlu_dist-32)-prefix)/.pkgcheck
-$(superlu_dist-32)-install: $($(superlu_dist-32)-prefix)/.pkginstall
-$(superlu_dist-32)-modulefile: $($(superlu_dist-32)-modulefile)
-$(superlu_dist-32)-clean:
-	rm -rf $($(superlu_dist-32)-modulefile)
-	rm -rf $($(superlu_dist-32)-prefix)
-	rm -rf $($(superlu_dist-32)-builddir)
-	rm -rf $($(superlu_dist-32)-srcdir)
-	rm -rf $($(superlu_dist-32)-src)
-$(superlu_dist-32): $(superlu_dist-32)-src $(superlu_dist-32)-unpack $(superlu_dist-32)-patch $(superlu_dist-32)-build $(superlu_dist-32)-check $(superlu_dist-32)-install $(superlu_dist-32)-modulefile
+$(superlu_dist-32-6.4.0)-src: $$($(superlu_dist-32-6.4.0)-src)
+$(superlu_dist-32-6.4.0)-unpack: $($(superlu_dist-32-6.4.0)-prefix)/.pkgunpack
+$(superlu_dist-32-6.4.0)-patch: $($(superlu_dist-32-6.4.0)-prefix)/.pkgpatch
+$(superlu_dist-32-6.4.0)-build: $($(superlu_dist-32-6.4.0)-prefix)/.pkgbuild
+$(superlu_dist-32-6.4.0)-check: $($(superlu_dist-32-6.4.0)-prefix)/.pkgcheck
+$(superlu_dist-32-6.4.0)-install: $($(superlu_dist-32-6.4.0)-prefix)/.pkginstall
+$(superlu_dist-32-6.4.0)-modulefile: $($(superlu_dist-32-6.4.0)-modulefile)
+$(superlu_dist-32-6.4.0)-clean:
+	rm -rf $($(superlu_dist-32-6.4.0)-modulefile)
+	rm -rf $($(superlu_dist-32-6.4.0)-prefix)
+	rm -rf $($(superlu_dist-32-6.4.0)-builddir)
+	rm -rf $($(superlu_dist-32-6.4.0)-srcdir)
+	rm -rf $($(superlu_dist-32-6.4.0)-src)
+$(superlu_dist-32-6.4.0): $(superlu_dist-32-6.4.0)-src $(superlu_dist-32-6.4.0)-unpack $(superlu_dist-32-6.4.0)-patch $(superlu_dist-32-6.4.0)-build $(superlu_dist-32-6.4.0)-check $(superlu_dist-32-6.4.0)-install $(superlu_dist-32-6.4.0)-modulefile
