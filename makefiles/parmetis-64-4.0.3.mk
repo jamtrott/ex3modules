c94196b18a2720219f58287cfaa845b6e5a649aa
James D. Trotter <4015365+jamtrott@users.noreply.github.com>
2023-05-22 11:17:00 +0200

    parmetis: ensure that correct version of METIS is found

--- makefiles/parmetis-64-4.0.3.mk
+++ makefiles/parmetis-64-4.0.3.mk
@@ -41,81 +41,6 @@ $($(parmetis-64)-prefix)/.pkgunpack: $$($(parmetis-64)-src) $($(parmetis-64)-src
 	tar -C $($(parmetis-64)-srcdir) --strip-components 1 -xz -f $<
 	@touch $@
 
-$($(parmetis-64)-srcdir)/0001-enable_external_metis.patch: $($(parmetis-64)-prefix)/.pkgunpack
-	@printf "" >$@.tmp
-	@echo 'diff --git a/CMakeLists.txt b/CMakeLists.txt' >>$@.tmp
-	@echo 'index ca945dd..aff8b5f 100644' >>$@.tmp
-	@echo '--- a/CMakeLists.txt' >>$@.tmp
-	@echo '+++ b/CMakeLists.txt' >>$@.tmp
-	@echo '@@ -23,7 +23,7 @@ else()' >>$@.tmp
-	@echo '   set(ParMETIS_LIBRARY_TYPE STATIC)' >>$@.tmp
-	@echo ' endif()' >>$@.tmp
-	@echo ' ' >>$@.tmp
-	@echo '-include($${GKLIB_PATH}/GKlibSystem.cmake)' >>$@.tmp
-	@echo '+include_directories($${GKLIB_PATH})' >>$@.tmp
-	@echo ' ' >>$@.tmp
-	@echo ' # List of paths that the compiler will search for header files.' >>$@.tmp
-	@echo ' # i.e., the -I equivalent' >>$@.tmp
-	@echo '@@ -33,7 +33,7 @@ include_directories($${GKLIB_PATH})' >>$@.tmp
-	@echo ' include_directories($${METIS_PATH}/include)' >>$@.tmp
-	@echo ' ' >>$@.tmp
-	@echo ' # List of directories that cmake will look for CMakeLists.txt' >>$@.tmp
-	@echo '-add_subdirectory($${METIS_PATH}/libmetis $${CMAKE_BINARY_DIR}/libmetis)' >>$@.tmp
-	@echo '+find_library(METIS_LIBRARY metis PATHS $${METIS_PATH}/lib)' >>$@.tmp
-	@echo ' add_subdirectory(include)' >>$@.tmp
-	@echo ' add_subdirectory(libparmetis)' >>$@.tmp
-	@echo ' add_subdirectory(programs)' >>$@.tmp
-	@echo 'diff --git a/libparmetis/CMakeLists.txt b/libparmetis/CMakeLists.txt' >>$@.tmp
-	@echo 'index 9cfc8a7..e0c4de7 100644' >>$@.tmp
-	@echo '--- a/libparmetis/CMakeLists.txt' >>$@.tmp
-	@echo '+++ b/libparmetis/CMakeLists.txt' >>$@.tmp
-	@echo '@@ -5,7 +5,10 @@ file(GLOB parmetis_sources *.c)' >>$@.tmp
-	@echo ' # Create libparmetis' >>$@.tmp
-	@echo ' add_library(parmetis $${ParMETIS_LIBRARY_TYPE} $${parmetis_sources})' >>$@.tmp
-	@echo ' # Link with metis and MPI libraries.' >>$@.tmp
-	@echo '-target_link_libraries(parmetis metis $${MPI_LIBRARIES})' >>$@.tmp
-	@echo '+target_link_libraries(parmetis $${METIS_LIBRARY} $${MPI_LIBRARIES})' >>$@.tmp
-	@echo '+if(UNIX)' >>$@.tmp
-	@echo '+  target_link_libraries(parmetis m)' >>$@.tmp
-	@echo '+endif()' >>$@.tmp
-	@echo ' set_target_properties(parmetis PROPERTIES LINK_FLAGS "$${MPI_LINK_FLAGS}")' >>$@.tmp
-	@echo ' ' >>$@.tmp
-	@echo ' install(TARGETS parmetis' >>$@.tmp
-	@echo 'diff --git a/libparmetis/parmetislib.h b/libparmetis/parmetislib.h' >>$@.tmp
-	@echo 'index c1daeeb..07511f6 100644' >>$@.tmp
-	@echo '--- a/libparmetis/parmetislib.h' >>$@.tmp
-	@echo '+++ b/libparmetis/parmetislib.h' >>$@.tmp
-	@echo '@@ -20,13 +20,12 @@' >>$@.tmp
-	@echo ' ' >>$@.tmp
-	@echo ' #include <parmetis.h>' >>$@.tmp
-	@echo ' ' >>$@.tmp
-	@echo '-#include "../metis/libmetis/gklib_defs.h"' >>$@.tmp
-	@echo '+#include <gklib_defs.h>' >>$@.tmp
-	@echo ' ' >>$@.tmp
-	@echo '-#include <mpi.h> ' >>$@.tmp
-	@echo '+#include <mpi.h>' >>$@.tmp
-	@echo ' ' >>$@.tmp
-	@echo ' #include <rename.h>' >>$@.tmp
-	@echo ' #include <defs.h>' >>$@.tmp
-	@echo ' #include <struct.h>' >>$@.tmp
-	@echo ' #include <macros.h>' >>$@.tmp
-	@echo ' #include <proto.h>' >>$@.tmp
-	@echo '-' >>$@.tmp
-	@echo 'diff --git a/programs/parmetisbin.h b/programs/parmetisbin.h' >>$@.tmp
-	@echo 'index e26cd2d..d156480 100644' >>$@.tmp
-	@echo '--- a/programs/parmetisbin.h' >>$@.tmp
-	@echo '+++ b/programs/parmetisbin.h' >>$@.tmp
-	@echo '@@ -19,7 +19,7 @@' >>$@.tmp
-	@echo ' #include <GKlib.h>' >>$@.tmp
-	@echo ' #include <parmetis.h>' >>$@.tmp
-	@echo ' ' >>$@.tmp
-	@echo '-#include "../metis/libmetis/gklib_defs.h"' >>$@.tmp
-	@echo '+#include <gklib_defs.h>' >>$@.tmp
-	@echo ' #include "../libparmetis/rename.h"' >>$@.tmp
-	@echo ' #include "../libparmetis/defs.h"' >>$@.tmp
-	@echo ' #include "../libparmetis/struct.h"' >>$@.tmp
-	@mv $@.tmp $@
-
 $($(parmetis-64)-srcdir)/0002-petsc-bugfix-1.patch: $($(parmetis-64)-prefix)/.pkgunpack
 	@printf "" >$@.tmp
 	@echo 'From 1c1a9fd0f408dc4d42c57f5c3ee6ace411eb222b Mon Sep 17 00:00:00 2001' >>$@.tmp
@@ -234,9 +159,9 @@ $($(parmetis-64)-srcdir)/0003-petsc-bugfix-2.patch: $($(parmetis-64)-prefix)/.pk
 	@echo '2.1.1.1.g1fb337f' >>$@.tmp
 	@mv $@.tmp $@
 
-$($(parmetis-64)-prefix)/.pkgpatch: $(modulefilesdir)/.markerfile $$(foreach dep,$$($(parmetis-64)-builddeps),$(modulefilesdir)/$$(dep)) $($(parmetis-64)-prefix)/.pkgunpack $($(parmetis-64)-srcdir)/0001-enable_external_metis.patch $($(parmetis-64)-srcdir)/0002-petsc-bugfix-1.patch $($(parmetis-64)-srcdir)/0003-petsc-bugfix-2.patch
+$($(parmetis-64)-prefix)/.pkgpatch: $(modulefilesdir)/.markerfile $$(foreach dep,$$($(parmetis-64)-builddeps),$(modulefilesdir)/$$(dep)) $($(parmetis-64)-prefix)/.pkgunpack $($(parmetis-64)-srcdir)/0002-petsc-bugfix-1.patch $($(parmetis-64)-srcdir)/0003-petsc-bugfix-2.patch
+	patch -d $($(parmetis-64)-srcdir) -f -p1 <patches/parmetis-4.0.3-0001-enable_external_metis.patch
 	cd $($(parmetis-64)-srcdir) && \
-		patch -f -p1 <0001-enable_external_metis.patch && \
 		patch -f -p1 <0002-petsc-bugfix-1.patch && \
 		patch -f -p1 <0003-petsc-bugfix-2.patch
 	sed -i 's,IDXTYPEWIDTH 32,IDXTYPEWIDTH 64,' $($(parmetis-64)-srcdir)/metis/include/metis.h
@@ -256,10 +181,11 @@ $($(parmetis-64)-prefix)/.pkgbuild: $(modulefilesdir)/.markerfile $$(foreach dep
 			-DCMAKE_INSTALL_LIBDIR=lib \
 			-DCMAKE_C_COMPILER="$${MPICC}" \
 			-DCMAKE_CXX_COMPILER="$${MPICXX}" \
+			-DCMAKE_BUILD_TYPE=Debug \
 			-DSHARED=1 \
-			-DMETIS_PATH=$${METIS_ROOT} \
-			-DGKLIB_PATH=$${GKLIB_ROOT} && \
-		$(MAKE)
+			-DMETIS_PATH="$${METIS_ROOT}" \
+			-DGKLIB_PATH="$${GKLIB_ROOT}" && \
+		$(MAKE) VERBOSE=1
 	@touch $@
 
 $($(parmetis-64)-prefix)/.pkgcheck: $(modulefilesdir)/.markerfile $$(foreach dep,$$($(parmetis-64)-builddeps),$(modulefilesdir)/$$(dep)) $($(parmetis-64)-builddir)/.markerfile $($(parmetis-64)-prefix)/.pkgbuild
